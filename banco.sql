6c59372391bad57ef46c2b5dcde72f6519b4cc68
Rodrigo Dellinghausen <dellinghausen@gmail.com>
2019-09-14 10:49:07 -0300

    criacao do banco

---
+++ banco.sql
@@ -0,0 +1,20 @@
+create table post (
+        id int PRIMARY KEY,
+        titulo varchar(100) NOT NULL,
+        subtitulo varchar(150),
+        texto varchar (4000) NOT NULL,
+        autor varchar (25) NOT NULL,
+        fonte varchar (400),
+        imagem varchar (250) NOT NULL,
+        data_criacao date NOT NULL
+        );
+        
+ create table comentario (
+        id int PRIMARY KEY,
+        autor varchar (35) NOT NULL,
+        texto varchar (350) NOT NULL,
+        data_coment timestamp NOT NULL, 
+        post_id int, 
+        status varchar (25) NOT NULL,
+        FOREIGN KEY (post_id) REFERENCES post (id)
+        );       
